2940f10973bca4daefc38bb9377ebada1b7f2e2f
Lea Rosema <lea.rosema@sinnerschrader.com>
2020-08-20 15:57:07 +0200

    chore: initial commit

---
+++ scripts/generate-fake-data.mjs
@@ -0,0 +1,18 @@
+import { bobRossLipsum } from './lib/bob-ross-lipsum.mjs';
+import fs from 'fs';
+
+const pick = (array) => array[(Math.random() * array.length) | 0];
+
+function createRandomMessage(id) {
+  return {
+    id: id || Math.random().toString(16).slice(2),
+    user: 'Bob Ross',
+    category: pick(['Arts', 'Design', 'Inspiration']),
+    message: bobRossLipsum(),
+  };
+}
+
+const data = Array(2000)
+  .fill(0)
+  .map((_, idx) => createRandomMessage(idx));
+fs.writeFileSync('data.json', JSON.stringify(data, null, 2), 'utf-8');
