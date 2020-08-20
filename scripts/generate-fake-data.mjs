4948e7eda94464469346bc079fe13b0bbb08d7a0
Lea Rosema <lea.rosema@sinnerschrader.com>
2020-08-20 16:56:41 +0200

    feat: timestamps

--- scripts/generate-fake-data.mjs
+++ scripts/generate-fake-data.mjs
@@ -4,11 +4,17 @@ import fs from 'fs';
 const pick = (array) => array[(Math.random() * array.length) | 0];
 
 function createRandomMessage(id) {
+  const thirtyDays = 30 * 86400;
+  const thirtyDaysAgo = new Date(new Date().getTime() - thirtyDays * 1e3);
+  const timestamp = new Date(
+    thirtyDaysAgo.getTime() + ((Math.random() * thirtyDays) | 0) * 1e3
+  ).toISOString();
   return {
     id: id || Math.random().toString(16).slice(2),
     user: 'Bob Ross',
     category: pick(['Arts', 'Design', 'Inspiration']),
     message: bobRossLipsum(),
+    timestamp,
   };
 }
 
